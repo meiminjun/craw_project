992856dcf2989bb982e5e20aee038f15fe534fc6
double-point <1010490079@qq.com>
2021-02-19 19:11:15 +0800

    爬取基金季度报告

---
+++ craw_fund_report/__init__.py
@@ -0,0 +1,23 @@
+# encoding:utf-8
+# FileName: __init__.py
+# Author:   xiaoyi | 小一
+# wechat:   zhiqiuxiaoyi
+# 公众号：   小一的学习笔记
+# email:    zhiqiuxiaoyi@qq.com
+# Date:     2021/2/18 16:33
+# Description: 爬取基金季度报告
+
+import pandas as pd
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
+
+# 显示所有列
+pd.set_option('display.max_columns', None)
+# 显示所有行
+# pd.set_option('display.max_rows', None)
+
+
+if __name__ == '__main__':
+    pass
